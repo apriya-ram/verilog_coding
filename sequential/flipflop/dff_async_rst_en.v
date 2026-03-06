e667d7fe71a820fa34d02edbce51c43375b2df80
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2026-03-06 20:08:29 +0530

    Create dff_async_rst_en.v

---
+++ sequential/flipflop/dff_async_rst_en.v
@@ -0,0 +1,18 @@
+//Dflipflop with enable and asynchronous reset 
+module dff_async_reset_en (
+    input d,
+    input clk,
+    input rst,
+    input en,          // Clock enable
+    output reg q
+);
+
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            q <= 1'b0;
+        else if (en)
+            q <= d;
+        // If en is 0, q holds its value
+    end
+
+endmodule
