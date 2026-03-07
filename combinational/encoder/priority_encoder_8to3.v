8adb20dfe4da56842aa58cfee2606998fca78470
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2026-03-07 21:43:45 +0530

    Create priority_encoder_8to3.v

---
+++ combinational/encoder/priority_encoder_8to3.v
@@ -0,0 +1,25 @@
+// 8-to-3 Priority Encoder
+module priority_encoder_8to3 (
+    input [7:0] din,
+    output reg [2:0] dout,
+    output reg valid
+);
+
+    always @(*) begin
+        casex (din)
+            8'b1xxxxxxx: dout = 3'b111;
+            8'b01xxxxxx: dout = 3'b110;
+            8'b001xxxxx: dout = 3'b101;
+            8'b0001xxxx: dout = 3'b100;
+            8'b00001xxx: dout = 3'b011;
+            8'b000001xx: dout = 3'b010;
+            8'b0000001x: dout = 3'b001;
+            8'b00000001: dout = 3'b000;
+            default: dout = 3'b000;
+        endcase
+        
+        // Valid output
+        valid = (din != 8'b00000000);
+    end
+
+endmodule
