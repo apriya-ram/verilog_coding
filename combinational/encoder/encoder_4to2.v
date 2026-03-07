805be3f3a840e9bf92388da3801f900a90aa698d
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2026-03-07 21:32:28 +0530

    Create encoder_4to2.v

---
+++ combinational/encoder/encoder_4to2.v
@@ -0,0 +1,17 @@
+// 4-to-2 Encoder
+module encoder_4to2 (
+    input [3:0] din,    // 4-bit input (only one bit should be high)
+    output reg [1:0] dout // 2-bit encoded output
+);
+
+    always @(*) begin
+        case (din)
+            4'b0001: dout = 2'b00;
+            4'b0010: dout = 2'b01;
+            4'b0100: dout = 2'b10;
+            4'b1000: dout = 2'b11;
+            default: dout = 2'b00; // Invalid input
+        endcase
+    end
+
+endmodule
